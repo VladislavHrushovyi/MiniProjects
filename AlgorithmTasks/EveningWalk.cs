8e55ed84a43e2782b2567f3b874acd9bcfa33c15
vladg <vlad.grush.99@gmail.com>
2024-03-06 11:44:13 +0200

    some bad test way to solving problem

--- AlgorithmTasks/EveningWalk.cs
+++ AlgorithmTasks/EveningWalk.cs
@@ -9,18 +9,34 @@ public class EveningWalk
         var inputArgs = inputArgsString.Split(" ").Select(long.Parse).ToArray();
 
         var minSumOfGrivnya = FindLowestAmountOfGrivnya(inputArgs[0], inputArgs[1], inputArgs[2]);
-        
+
         Console.WriteLine(minSumOfGrivnya);
     }
 
     private long FindLowestAmountOfGrivnya(long a, long b, long c)
     {
-        long x = (b * c) / (b - a);
-        long iter = b / a;
-        while ((x / a) - (x / b) < c)
+        long x = 1;
+        long delta = 0;
+
+        while (true)
         {
-            x+=iter;
+            long left = x / a - x / b;
+            long right = c;
+
+            if (left - right > c)
+            {
+                return x;
+            }
+
+            if (left - right > delta)
+            {
+                delta = left - right;
+                x++;
+            }
+            else
+            {
+                x += delta - (left - right);
+            }
         }
-        return x;
     }
 }
