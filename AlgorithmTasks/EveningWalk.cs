1c87d3c557827aafd6d7492bf65cd67c1c42366f
vladg <vlad.grush.99@gmail.com>
2024-03-10 16:48:37 +0200

    idk how to solve this task

--- AlgorithmTasks/EveningWalk.cs
+++ AlgorithmTasks/EveningWalk.cs
@@ -4,23 +4,35 @@ public class EveningWalk
 {
     public void StartTasks()
     {
-        var inputArgsString = Console.ReadLine();
+        //var inputArgsString = Console.ReadLine();
 
-        var inputArgs = inputArgsString.Split(" ").Select(long.Parse).ToArray();
+        //var inputArgs = inputArgsString.Split(" ").Select(long.Parse).ToArray();
 
-        var minSumOfGrivnya = FindLowestAmountOfGrivnya(inputArgs[0], inputArgs[1], inputArgs[2]);
+        var case1 = FindLowestAmountOfGrivnya(3, 4, 2);
+        var case2 = FindLowestAmountOfGrivnya(600000, 700000, 800000);
+        var case3 = FindLowestAmountOfGrivnya(999998, 999999, 999999);
 
-        Console.WriteLine(minSumOfGrivnya);
+        Console.WriteLine($"CASE 1: a=3 b=4 c=2 X={case1}");
+        Console.WriteLine($"CASE 2: a=600000 b=700000 c=800000 X={case2}");
+        Console.WriteLine($"CASE 3: a=999998 b=999999 c=999999 X={case3}");
     }
 
     private static long FindLowestAmountOfGrivnya(long a, long b, long c)
     {
-        long sum = a * c;
-        while (sum / a - sum / b != c)
+        long minAmountDollars = c;
+        long spendMoney;
+        while (true)
         {
-            sum += a;
+            spendMoney = minAmountDollars * a;
+            long euros = spendMoney / b;
+            if (minAmountDollars % euros == c)
+            {
+                break;
+            }
+
+            minAmountDollars += a;
         }
 
-        return sum;
+        return spendMoney;
     }
 }
