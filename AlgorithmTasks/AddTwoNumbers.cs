c526e19d315beba44d317830b01ab95f06ac493f
vladg <vlad.grush.99@gmail.com>
2024-03-15 15:57:53 +0200

    LeetCode 2. Add Two Numbers (recursion approach)

--- AlgorithmTasks/AddTwoNumbers.cs
+++ AlgorithmTasks/AddTwoNumbers.cs
@@ -48,85 +48,14 @@ public class AddTwoNumbers
  }
 public class Solution 
 {
-    public ListNode AddTwoNumbers(ListNode l1, ListNode l2) 
+    public ListNode AddTwoNumbers(ListNode l1, ListNode l2, int remainder = 0) 
     {
-        ListNode result = null;
-        int sumRemainder = 0;
-        while (true)
-        {
-            if (l1 == null && l2 == null)
-            {
-                if (sumRemainder != 0)
-                {
-                    result = AddNode(result, sumRemainder);
-                }
-                break;
-            }
-            if (l1 != null && l2 != null)
-            {
-                int tempNodeSum = l1.Value + l2.Value + sumRemainder;
-                sumRemainder = tempNodeSum / 10;
-                int value = tempNodeSum > 9 ? tempNodeSum % (sumRemainder * 10) : tempNodeSum;
-                result = AddNode(result, value);
-            }
-            else if(l1 != null)
-            {
-                int tempNodeSum = l1.Value + sumRemainder;
-                sumRemainder = tempNodeSum / 10;
-                int value = tempNodeSum > 9 ? tempNodeSum % (sumRemainder * 10) : tempNodeSum;
-                result = AddNode(result, value);
-            }else if(l2 != null)
-            {
-                int tempNodeSum = l2.Value + sumRemainder;
-                sumRemainder = tempNodeSum / 10;
-                int value = tempNodeSum > 9 ? tempNodeSum % (sumRemainder * 10) : tempNodeSum;
-                result = AddNode(result, value);
-            }
+        if(l1 == null && l2 == null && remainder == 0) return null;
 
-            if (l1 != null)
-            {
-                if (l1.Next != null)
-                {
-                    l1 = l1.Next;
-                }
-                else
-                {
-                    l1 = null;
-                }
-            }
+        int sum = (l1 != null ? l1.Value : 0) + (l2 != null ? l2.Value : 0) + remainder;
 
-            if (l2 != null)
-            {
-                if (l2.Next != null)
-                {
-                    l2 = l2.Next;
-                }
-                else
-                {
-                    l2 = null;
-                }
-            }
-        }
-        return result;
-    }
+        remainder = sum / 10;
 
-    private ListNode AddNode(ListNode? list, int value)
-    {
-        var newNode = new ListNode(value);
-        var last = list;
-        if (last == null)
-        {
-            last = newNode;
-            list = last;
-            return list;
-        }
-        while (last?.Next != null)
-        {
-            last = last.Next;
-        }
-
-        last.Next = newNode;
-
-        return list;
+        return new ListNode(sum % 10, AddTwoNumbers(l1?.Next, l2?.Next, remainder));
     }
 }
