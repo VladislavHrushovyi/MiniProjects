e88782b5a711be71f4c2a6ddce93c5590faeb0e8
vladg <vlad.grush.99@gmail.com>
2024-03-20 15:10:10 +0200

    LeetCode 10.Regular Expression Matching

--- AlgorithmTasks/Program.cs
+++ AlgorithmTasks/Program.cs
@@ -1,4 +1,4 @@
 ﻿using AlgorithmTasks;
 
-var task = new PalindromeNumber();
+var task = new RegexMatching();
 task.Execute();
---
+++ AlgorithmTasks/RegexMatching.cs
@@ -0,0 +1,18 @@
+﻿using System.Text.RegularExpressions;
+namespace AlgorithmTasks;
+
+public class RegexMatching
+{
+    public void Execute()
+    {
+        var sol = new Solution();
+        var result = sol.IsMatch("aa", "a*");
+        Console.WriteLine(result);
+    }
+}
+
+partial class Solution {
+    public bool IsMatch(string s, string p) {
+        return Regex.IsMatch(s, "^"+p+"$");
+    }
+}
