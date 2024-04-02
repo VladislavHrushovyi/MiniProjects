8c717fd43f59d1bf3e8023ac908e5b406fc50554
vladg <vlad.grush.99@gmail.com>
2024-04-02 13:23:08 +0300

    LeetCode 26.Remove Duplicates from Sorted Array

---
+++ AlgorithmTasks/26-50/RemoveDuplicatesFromSortedArray.cs
@@ -0,0 +1,31 @@
+﻿namespace AlgorithmTasks._26_50;
+
+public class RemoveDuplicatesFromSortedArray
+{
+    public void Execute()
+    {
+        var sol = new Solution();
+        var result = sol.RemoveDuplicates(new[] { 1, 2, 3, 4, 4, 4, 5, 6, 6, 6, 6 });
+        Console.WriteLine(result);
+    }
+}
+
+partial class Solution {
+    public int RemoveDuplicates(int[] nums) {
+        if (nums.Length == 0)
+        {
+            return 0;
+        }
+
+        int j = 0;
+        for (int i = 1; i < nums.Length; i++)
+        {
+            if (nums[i] != nums[j])
+            {
+                nums[++j] = nums[i];
+            }
+        }
+
+        return j+1;
+    }
+}
