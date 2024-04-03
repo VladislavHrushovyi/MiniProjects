baa6b77709ee60b6fba1a58830df1d670bf32a71
vladg <vlad.grush.99@gmail.com>
2024-04-03 13:44:05 +0300

    Leetcode 28. Find the Index of the First Occurrence in a String

---
+++ AlgorithmTasks/26-50/FirstEntryIndexStrInStr.cs
@@ -0,0 +1,52 @@
+﻿namespace AlgorithmTasks._26_50;
+
+public class FirstEntryIndexStrInStr
+{
+    public void Execute()
+    {
+        var sol = new Solution();
+        var result = sol.StrStr("abc", "c");
+        Console.WriteLine(result);
+    }
+}
+
+partial class Solution {
+    public int StrStr(string haystack, string needle)
+    {
+        if (string.IsNullOrEmpty(haystack) || needle.Length > haystack.Length)
+        {
+            return -1;
+        }
+
+        if (haystack == needle)
+        {
+            return 0;
+        }
+
+        for (int i = 0; i < haystack.Length; i++)
+        {
+            if (IsSubString(i, haystack, needle))
+            {
+                return i;
+            }
+        }
+        return -1;
+    }
+
+    private bool IsSubString(int startIndex, string haystack, string needle)
+    {
+        if (needle.Length + startIndex> haystack.Length)
+        {
+            return false;
+        }
+        for (int i = 0; i < needle.Length; i++)
+        {
+            if (haystack[startIndex + i] != needle[i])
+            {
+                return false;
+            }
+        }
+
+        return true;
+    }
+}
--- AlgorithmTasks/Program.cs
+++ AlgorithmTasks/Program.cs
@@ -1,5 +1,5 @@
 ﻿using AlgorithmTasks;
 using AlgorithmTasks._26_50;
 
-var task = new RemoveElement();
+var task = new FirstEntryIndexStrInStr();
 task.Execute();
