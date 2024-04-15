50ece2ca2ca5371544f539bdf1406da5e4d20606
vladg <vlad.grush.99@gmail.com>
2024-04-15 20:11:24 +0300

    Task 38 preparing and research solution

---
+++ AlgorithmTasks/26-50/CountAndSay.cs
@@ -0,0 +1,28 @@
+﻿namespace AlgorithmTasks._26_50;
+
+public class CountAndSay
+{
+    public void Execute()
+    {
+        var sol = new Solution();
+        var result = sol.CountAndSay(5);
+        
+        Console.WriteLine(result);
+    }
+}
+
+partial class Solution {
+    public string CountAndSay(int n)
+    {
+        if (n == 1)
+        {
+            return "1";
+        }
+        return Say(CountAndSay(n - 1));
+    }
+
+    private string Say(string s)
+    {
+        return String.Empty;
+    }
+}
