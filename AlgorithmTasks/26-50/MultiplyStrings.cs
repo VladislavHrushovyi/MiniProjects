d23e1524253a96000d9926ad5582c52400e3d1a8
vladg <vlad.grush.99@gmail.com>
2024-04-24 16:15:34 +0300

    preparing to task 43

---
+++ AlgorithmTasks/26-50/MultiplyStrings.cs
@@ -0,0 +1,18 @@
+﻿namespace AlgorithmTasks._26_50;
+
+public class MultiplyStrings
+{
+    public void Execute()
+    {
+        var sol = new Solution();
+        var result = sol.Multiply("3", "2");
+        Console.WriteLine(result);
+    }
+}
+
+partial class Solution {
+    public string Multiply(string num1, string num2)
+    {
+        return "";
+    }
+}
