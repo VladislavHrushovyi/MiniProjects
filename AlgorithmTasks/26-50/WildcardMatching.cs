75d8e41757b2b3e9864c1f4a62e4d574eeae7ca2
vladg <vlad.grush.99@gmail.com>
2024-04-26 20:07:27 +0300

    preparing to task 44

---
+++ AlgorithmTasks/26-50/WildcardMatching.cs
@@ -0,0 +1,19 @@
+﻿namespace AlgorithmTasks._26_50;
+
+public class WildcardMatching
+{
+    public void Execute()
+    {
+        var sol = new Solution();
+        var res = sol.IsMatch("aa", "a");
+        Console.WriteLine(res);
+    }
+}
+
+partial class Solution {
+    public bool IsMatch(string s, string p)
+    {
+        
+        return true;
+    }
+}
