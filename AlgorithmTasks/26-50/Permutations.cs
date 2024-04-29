7529239881b5ed232223ca1724cfde59959b78a8
vladg <vlad.grush.99@gmail.com>
2024-04-29 20:44:16 +0300

    preparing to 46 task

---
+++ AlgorithmTasks/26-50/Permutations.cs
@@ -0,0 +1,26 @@
+﻿using System.Text;
+
+namespace AlgorithmTasks._26_50;
+
+public class Permutations
+{
+    public void Execute()
+    {
+        var sol = new Solution();
+        var result = sol.Permute(new[] { 1,2,3 });
+        var sb = new StringBuilder();
+        foreach (var row in result)
+        {
+            sb.Append($"[{string.Join(",", row)}] ");
+        }
+        
+        Console.WriteLine(sb);
+    }
+}
+
+partial class Solution {
+    public IList<IList<int>> Permute(int[] nums)
+    {
+        return new List<IList<int>>();
+    }
+}
