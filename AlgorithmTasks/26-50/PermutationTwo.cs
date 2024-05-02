6fe6c6be97074a28249d038f49a7b50d7354c262
vladg <vlad.grush.99@gmail.com>
2024-05-02 16:53:29 +0300

    preparing to task 47

---
+++ AlgorithmTasks/26-50/PermutationTwo.cs
@@ -0,0 +1,26 @@
+﻿using System.Text;
+
+namespace AlgorithmTasks._26_50;
+
+public class PermutationTwo
+{
+    public void Execute()
+    {
+        var sol = new Solution();
+        var result = sol.PermuteUnique(new[] { 1, 2, 3 });
+        var sb = new StringBuilder();
+        foreach (var row in result)
+        {
+            sb.Append($"[{string.Join(",", row)}] ");
+        }
+
+        Console.WriteLine(sb);
+    }
+}
+
+partial class Solution {
+    public IList<IList<int>> PermuteUnique(int[] nums)
+    {
+        return new List<IList<int>>();
+    }
+}
