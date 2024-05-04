b428e8d8f8ec004447a32294a38dea60aead237c
vladg <vlad.grush.99@gmail.com>
2024-05-04 19:41:10 +0300

    preparing to task 48

---
+++ AlgorithmTasks/26-50/RotateImage.cs
@@ -0,0 +1,26 @@
+﻿namespace AlgorithmTasks._26_50;
+
+public class RotateImage
+{
+    public void Execute()
+    {
+        var image = new int[][]
+        {
+            new int[] {1, 2, 3},
+            new int[] {4, 5, 6},
+            new int[] {7, 8 ,9}
+        };
+        var sol = new Solution();
+        sol.Rotate(image);
+        foreach (var line in image)
+        {
+            Console.WriteLine(string.Join(", ", line));
+        }
+    }
+}
+
+partial class Solution {
+    public void Rotate(int[][] matrix) {
+        
+    }
+}
