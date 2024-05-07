04f82dd73e5abf1fa33c23b63675c3255b324828
vladg <vlad.grush.99@gmail.com>
2024-05-07 19:38:52 +0300

    Preparing to task 49

---
+++ AlgorithmTasks/26-50/GroupAnagrams.cs
@@ -0,0 +1,23 @@
+﻿namespace AlgorithmTasks._26_50;
+
+public class GroupAnagrams
+{
+    public void Execute()
+    {
+        var sol = new Solution();
+        var result = sol.GroupAnagrams(new[] { "eat","tea","tan","ate","nat","bat" });
+        foreach (var line in result)
+        {
+            Console.WriteLine("(" + string.Join(", ", line) + ")");
+        }
+    }
+}
+
+partial class Solution {
+    public IList<IList<string>> GroupAnagrams(string[] strs)
+    {
+        IList<IList<string>> results = new List<IList<string>>();
+
+        return results;
+    }
+}
