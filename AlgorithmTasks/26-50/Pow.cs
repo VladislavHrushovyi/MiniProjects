c67424a7a0f66a050952532996248d4f823e749d
vladg <vlad.grush.99@gmail.com>
2024-05-09 20:23:53 +0300

    preparing to task 50

---
+++ AlgorithmTasks/26-50/Pow.cs
@@ -0,0 +1,18 @@
+﻿namespace AlgorithmTasks._26_50;
+
+public class Pow
+{
+    public void Execute()
+    {
+        var sol = new Solution();
+        var result = sol.MyPow(2, 10);
+        Console.WriteLine(result);
+    }
+}
+
+partial class Solution {
+    public double MyPow(double x, int n)
+    {
+        return 0d;
+    }
+}
