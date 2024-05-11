afebe5b27032f6000f60da3e07e62b7f4ee0c299
vladg <vlad.grush.99@gmail.com>
2024-05-11 18:20:50 +0300

    preparing to task 51

---
+++ AlgorithmTasks/51-75/NQueens.cs
@@ -0,0 +1,21 @@
+﻿namespace AlgorithmTasks._51_75;
+
+public class NQueens
+{
+    public void Execute()
+    {
+        var sol = new Solution();
+        var result = sol.SolveNQueens(4);
+        foreach (var line in result)
+        {
+            Console.WriteLine("(" + string.Join(",", line) + ")");
+        }
+    }
+}
+
+partial class Solution {
+    public IList<IList<string>> SolveNQueens(int n)
+    {
+        return new List<IList<string>>();
+    }
+}
