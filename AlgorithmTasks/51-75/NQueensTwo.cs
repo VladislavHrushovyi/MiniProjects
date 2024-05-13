bff88a9662d0a6f4a76d60e2f12c75aa47d5d586
vladg <vlad.grush.99@gmail.com>
2024-05-13 19:29:11 +0300

    preparing to task 52

---
+++ AlgorithmTasks/51-75/NQueensTwo.cs
@@ -0,0 +1,19 @@
+﻿namespace AlgorithmTasks._51_75;
+
+public class NQueensTwo
+{
+    public void Execute()
+    {
+        var sol = new Solution();
+        var result = sol.TotalNQueens(4);
+        Console.WriteLine(result);
+    }
+}
+
+partial class Solution {
+    public int TotalNQueens(int n)
+    {
+        int result = 0;
+        return result;
+    }
+}
