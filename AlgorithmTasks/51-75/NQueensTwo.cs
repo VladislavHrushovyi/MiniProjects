40876d9d4f444d57c4d87cf5730b7d95ba7425ca
vladg <vlad.grush.99@gmail.com>
2024-05-14 20:37:38 +0300

    LeetCode 52. N-Queens II

--- AlgorithmTasks/51-75/NQueensTwo.cs
+++ AlgorithmTasks/51-75/NQueensTwo.cs
@@ -13,7 +13,30 @@ public class NQueensTwo
 partial class Solution {
     public int TotalNQueens(int n)
     {
-        int result = 0;
-        return result;
+        int count = 0;
+        var rows = new bool[n];
+        var diag1 = new bool[2 * n];
+        var diag2 = new bool[2 * n];
+
+        Helper(0);
+
+        return count;
+
+        void Helper(int col)
+        {
+            if (col == n) count++;
+
+            for (int row = 0; row < n; row++)
+            {
+                int a = row + col;
+                int b = row - col + n;
+
+                if (rows[row] || diag1[a] || diag2[b]) continue; 
+
+                rows[row] = diag1[a] = diag2[b] = true; 
+                Helper(col + 1);
+                rows[row] = diag1[a] = diag2[b] = false;
+            }
+        }
     }
 }
--- AlgorithmTasks/Program.cs
+++ AlgorithmTasks/Program.cs
@@ -1,5 +1,5 @@
 ﻿using AlgorithmTasks._26_50;
 using AlgorithmTasks._51_75;
 
-var task = new NQueens();
+var task = new NQueensTwo();
 task.Execute();
