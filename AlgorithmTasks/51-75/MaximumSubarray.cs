2a843243c5d8cff6b0b13f28105a1ca91ff5c539
vladg <vlad.grush.99@gmail.com>
2024-05-15 19:42:59 +0300

    preparing to task 53

---
+++ AlgorithmTasks/51-75/MaximumSubarray.cs
@@ -0,0 +1,19 @@
+﻿namespace AlgorithmTasks._51_75;
+
+public class MaximumSubarray
+{
+    public void Execute()
+    {
+        var sol = new Solution();
+        var result = sol.MaxSubArray(new[] { -2, 1, -3, 4, -1, 2, 1, -5, 4 });
+    }
+}
+
+partial class Solution {
+    public int MaxSubArray(int[] nums)
+    {
+        int length = 0;
+
+        return length;
+    }
+}
