b721104658685819a73d103acf20976650fa0f50
vladg <vlad.grush.99@gmail.com>
2024-05-18 19:28:08 +0300

    preparing to task 54

---
+++ AlgorithmTasks/51-75/SpiralMatrix.cs
@@ -0,0 +1,24 @@
+﻿namespace AlgorithmTasks._51_75;
+
+public class SpiralMatrix
+{
+    public void Execute()
+    {
+        var data = new int[][]
+        {
+            new[] { 1, 2, 3 },
+            new[] { 4, 5, 6 },
+            new[] { 7, 8, 9 },
+        };
+        var sol = new Solution();
+        var result = sol.SpiralOrder(data);
+        Console.WriteLine(string.Join(",", result));
+    }
+}
+
+partial class Solution {
+    public IList<int> SpiralOrder(int[][] matrix)
+    {
+        return new List<int>();
+    }
+}
