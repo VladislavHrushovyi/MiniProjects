2e7780b7b56d16af11127b2ddbafbe8262234576
vladg <vlad.grush.99@gmail.com>
2024-05-19 20:37:24 +0300

    LeetCode 54. Spiral Matrix

--- AlgorithmTasks/51-75/SpiralMatrix.cs
+++ AlgorithmTasks/51-75/SpiralMatrix.cs
@@ -19,6 +19,32 @@ public class SpiralMatrix
 partial class Solution {
     public IList<int> SpiralOrder(int[][] matrix)
     {
-        return new List<int>();
+        IList<int> res = new List<int>();
+        int top = 0, left = 0, right = matrix[0].Length - 1, bot = matrix.Length - 1, elements = matrix.Length * matrix[0].Length;
+
+        while (res.Count < elements) {
+            for (int i = left; i <= right && res.Count < elements; i++) {
+                res.Add(matrix[top][i]);
+            }
+            top++;
+
+            for (int j = top; j <= bot && res.Count < elements; j++) {
+                res.Add(matrix[j][right]);
+            }
+            right--;
+
+            for (int i = right; i >= left && res.Count < elements; i--) {
+                res.Add(matrix[bot][i]);
+            }
+            bot--;
+
+            for (int i = bot; i >= top && res.Count < elements; i--) {
+                res.Add(matrix[i][left]);
+            }
+
+            left++;
+        }
+
+        return res;
     }
 }
--- AlgorithmTasks/Program.cs
+++ AlgorithmTasks/Program.cs
@@ -1,5 +1,5 @@
 ﻿using AlgorithmTasks._26_50;
 using AlgorithmTasks._51_75;
 
-var task = new MaximumSubarray();
+var task = new SpiralMatrix();
 task.Execute();
