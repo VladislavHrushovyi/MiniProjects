3e8f67c27589314718efff184a39552dc5c38803
vladg <vlad.grush.99@gmail.com>
2024-05-22 19:31:54 +0300

    LeetCode 55. Jump Game

--- AlgorithmTasks/51-75/JumpGame.cs
+++ AlgorithmTasks/51-75/JumpGame.cs
@@ -13,6 +13,15 @@ public class JumpGame
 partial class Solution {
     public bool CanJump(int[] nums)
     {
-        return true;
+        int finishIndex = nums.Length - 1;
+        for (int i = nums.Length - 1; i >= 0; i--)
+        {
+            if (i + nums[i] >= finishIndex)
+            {
+                if (i == 0) return true;
+                finishIndex = i;
+            }
+        }
+        return false;
     }
 }
