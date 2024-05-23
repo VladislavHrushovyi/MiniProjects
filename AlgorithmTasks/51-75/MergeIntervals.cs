a21faafaac74d89313ec9b93b3ce8ddb2552a787
vladg <vlad.grush.99@gmail.com>
2024-05-23 20:57:48 +0300

    preparing to task 56

---
+++ AlgorithmTasks/51-75/MergeIntervals.cs
@@ -0,0 +1,32 @@
+﻿namespace AlgorithmTasks._51_75;
+
+public class MergeIntervals
+{
+    public void Execute()
+    {
+        var data = new int[][]
+        {
+            new int[]{1,3},
+            new int[]{2,6},
+            new int[]{8,10},
+            new int[]{15,18},
+        };
+        
+        var sol = new Solution();
+        var result = sol.Merge(data);
+
+        Console.Write("[");
+        foreach (var line in result)
+        {
+            Console.Write("["+string.Join(",", line)+"]");
+        }
+        Console.WriteLine("]");
+    }
+}
+
+partial class Solution {
+    public int[][] Merge(int[][] intervals)
+    {
+        return new[] { new[] {1} };
+    }
+}
