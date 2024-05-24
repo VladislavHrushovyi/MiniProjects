7be752c9428887081cd6385ba6a45c55c130046a
vladg <vlad.grush.99@gmail.com>
2024-05-24 19:46:00 +0300

    LeetCode 56. Merge Intervals

--- AlgorithmTasks/51-75/MergeIntervals.cs
+++ AlgorithmTasks/51-75/MergeIntervals.cs
@@ -6,27 +6,45 @@ public class MergeIntervals
     {
         var data = new int[][]
         {
-            new int[]{1,3},
-            new int[]{2,6},
-            new int[]{8,10},
-            new int[]{15,18},
+            new int[] { 1, 3 },
+            new int[] { 2, 6 },
+            new int[] { 8, 10 },
+            new int[] { 15, 18 },
         };
-        
+
         var sol = new Solution();
         var result = sol.Merge(data);
 
         Console.Write("[");
         foreach (var line in result)
         {
-            Console.Write("["+string.Join(",", line)+"]");
+            Console.Write("[" + string.Join(",", line) + "]");
         }
+
         Console.WriteLine("]");
     }
 }
 
-partial class Solution {
+partial class Solution
+{
     public int[][] Merge(int[][] intervals)
     {
-        return new[] { new[] {1} };
+        List<int[]> output = new List<int[]>();
+
+        Array.Sort(intervals, (a, b) => a[0] - b[0]);
+
+        output.Add(intervals[0]);
+
+        for (int i = 1; i < intervals.Length; i++)
+        {
+            if (output[^1][1] >= intervals[i][0])
+            {
+                if (output[^1][1] <= intervals[i][1])
+                    output[^1][1] = intervals[i][1];
+            }
+            else output.Add(intervals[i]);
+        }
+
+        return output.ToArray();
     }
 }
--- AlgorithmTasks/Program.cs
+++ AlgorithmTasks/Program.cs
@@ -1,5 +1,5 @@
 ﻿using AlgorithmTasks._26_50;
 using AlgorithmTasks._51_75;
 
-var task = new JumpGame();
+var task = new MergeIntervals();
 task.Execute();
