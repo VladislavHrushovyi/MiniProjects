f67a41bb2860ab82202d3b69b2dbd1e11235e09a
vladg <vlad.grush.99@gmail.com>
2024-05-25 23:08:36 +0300

    preparing to task 57

---
+++ AlgorithmTasks/51-75/InsertInterval.cs
@@ -0,0 +1,30 @@
+﻿namespace AlgorithmTasks._51_75;
+
+public class InsertInterval
+{
+    public void Execute()
+    {
+        var intervals = new int[][]
+        {
+            new[] { 1, 3 },
+            new[] { 6, 9 }
+        };
+        var newInterval = new[] { 2, 5 };
+        var sol = new Solution();
+        var result = sol.Insert(intervals, newInterval);
+        
+        Console.Write("[");
+        foreach (var line in result)
+        {
+            Console.Write("["+string.Join(", ", line)+"]");
+        }
+        Console.WriteLine("]");
+    }
+}
+
+partial class Solution {
+    public int[][] Insert(int[][] intervals, int[] newInterval)
+    {
+        return new[] { new[] {1} };
+    }
+}
