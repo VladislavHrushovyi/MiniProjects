7e410ccd8e37deb474cdd758043caa868dd442ba
vladg <vlad.grush.99@gmail.com>
2024-05-27 20:23:52 +0300

    preparing to task 58

---
+++ AlgorithmTasks/51-75/LengthOfLastWord.cs
@@ -0,0 +1,18 @@
+﻿namespace AlgorithmTasks._51_75;
+
+public class LengthOfLastWord
+{
+    public void Execute()
+    {
+        var sol = new Solution();
+        var result = sol.LengthOfLastWord("Hello World");
+        Console.WriteLine(result);
+    }
+}
+
+partial class Solution {
+    public int LengthOfLastWord(string s)
+    {
+        return 0;
+    }
+}
