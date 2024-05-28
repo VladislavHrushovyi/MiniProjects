1273635b07ae4a76eb599312d08d06520c459b32
vladg <vlad.grush.99@gmail.com>
2024-05-28 20:17:33 +0300

    LeetCode 58. Length of Last Word

--- AlgorithmTasks/51-75/LengthOfLastWord.cs
+++ AlgorithmTasks/51-75/LengthOfLastWord.cs
@@ -13,6 +13,18 @@ public class LengthOfLastWord
 partial class Solution {
     public int LengthOfLastWord(string s)
     {
-        return 0;
+        s = s.Trim();
+        
+        int length = 0;
+        for (int i = s.Length - 1; i >= 0; i--) {
+            if (s[i] != ' ') {
+                length++;
+            }
+            else if (length > 0) {
+                break;
+            }
+        }
+        
+        return length;
     }
 }
