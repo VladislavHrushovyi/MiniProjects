3e8a035d93840cc3d7d977f1a1fd6f3a07a107c3
vladg <vlad.grush.99@gmail.com>
2024-05-29 19:38:17 +0300

    preparing to task 59

---
+++ AlgorithmTasks/51-75/SpiralMatrixTwo.cs
@@ -0,0 +1,25 @@
+﻿namespace AlgorithmTasks._51_75;
+
+public class SpiralMatrixTwo
+{
+    public void Execute()
+    {
+        int n = 3;
+        var sol = new Solution();
+        var result = sol.GenerateMatrix(n);
+        
+        Console.Write("[");
+        foreach (var line in result)
+        {
+            Console.Write("["+string.Join(",", line)+"]");
+        }
+        Console.WriteLine("]");
+    }
+}
+
+partial class Solution {
+    public int[][] GenerateMatrix(int n)
+    {
+        return new[] { new[] { 1 } };
+    }
+}
