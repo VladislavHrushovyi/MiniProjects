fb3ea4df1d43267b98faa74f89cec806d57d98bc
vladg <vlad.grush.99@gmail.com>
2024-05-30 19:43:46 +0300

    LeetCode 59. Spiral Matrix II

--- AlgorithmTasks/51-75/SpiralMatrixTwo.cs
+++ AlgorithmTasks/51-75/SpiralMatrixTwo.cs
@@ -20,6 +20,40 @@ public class SpiralMatrixTwo
 partial class Solution {
     public int[][] GenerateMatrix(int n)
     {
-        return new[] { new[] { 1 } };
+        int[][] matrix = new int[n][];
+        int count = 1;
+        int left = 0, right = n - 1, top = 0, bottom = n - 1;
+
+        for (int i = 0; i < n; i++)
+            matrix[i] = new int[n];
+
+        while (count < n*n + 1) 
+        {
+            for (int j = left; j <= right; j++) {
+                matrix[top][j] = count++;
+            }
+            top++;
+
+            for (int i = top; i <= bottom; i++) {
+                matrix[i][right] = count++;
+            }
+            right--;
+
+            if (top <= bottom) {
+                for (int j = right; j >= left; j--) {
+                    matrix[bottom][j] = count++;
+                }
+                bottom--;
+            }
+
+            if (left <= right) {
+                for (int i = bottom; i >= top; i--) {
+                    matrix[i][left] = count++;
+                }
+                left++;
+            }
+        }
+
+        return matrix;
     }
 }
--- AlgorithmTasks/Program.cs
+++ AlgorithmTasks/Program.cs
@@ -1,5 +1,5 @@
 ﻿using AlgorithmTasks._26_50;
 using AlgorithmTasks._51_75;
 
-var task = new LengthOfLastWord();
+var task = new SpiralMatrixTwo();
 task.Execute();
