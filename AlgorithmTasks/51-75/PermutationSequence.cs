17d660e094345a9331ba03424a91666df171695a
vladg <vlad.grush.99@gmail.com>
2024-05-31 20:04:58 +0300

    preparing to task 60

---
+++ AlgorithmTasks/51-75/PermutationSequence.cs
@@ -0,0 +1,17 @@
+﻿namespace AlgorithmTasks._51_75;
+
+public class PermutationSequence
+{
+    public void Execute()
+    {
+        var sol = new Solution();
+        var result = sol.GetPermutation(3, 3);
+        Console.WriteLine(result);
+    }
+}
+
+partial class Solution {
+    public string GetPermutation(int n, int k) {
+        return String.Empty;
+    }
+}
