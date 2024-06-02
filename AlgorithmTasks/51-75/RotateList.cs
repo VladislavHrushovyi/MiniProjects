28ab91b6780ee9185cd1b50b5447a03f377ce9b2
vladg <vlad.grush.99@gmail.com>
2024-06-02 19:57:35 +0300

    preparing to task 61

---
+++ AlgorithmTasks/51-75/RotateList.cs
@@ -0,0 +1,28 @@
+﻿namespace AlgorithmTasks._51_75;
+
+public class RotateList
+{
+    public void Execute()
+    {
+        ListNode node5 = new ListNode(5);
+        ListNode node4 = new ListNode(4, node5);
+        ListNode node3 = new ListNode(3, node4);
+        ListNode node2 = new ListNode(2, node3);
+        ListNode node1 = new ListNode(1, node2);
+        var sol = new Solution();
+        var result = sol.RotateRight(node1, 2);
+
+        while (result != null)
+        {
+            Console.Write(result.val +" -> ");
+            result = result.next;
+        }
+    }
+}
+
+partial class Solution {
+    public ListNode RotateRight(ListNode head, int k)
+    {
+        return new ListNode();
+    }
+}
