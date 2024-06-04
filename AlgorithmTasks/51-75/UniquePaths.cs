f5e74771a25ec7d6e6d78688acccc5f4827c69df
vladg <vlad.grush.99@gmail.com>
2024-06-04 20:23:26 +0300

    preparing to task 62

---
+++ AlgorithmTasks/51-75/UniquePaths.cs
@@ -0,0 +1,18 @@
+﻿namespace AlgorithmTasks._51_75;
+
+public class UniquePaths
+{
+    public void Execute()
+    {
+        var sol = new Solution();
+        var result = sol.UniquePaths(3,7);
+        Console.WriteLine(result);
+    }
+}
+
+partial class Solution {
+    public int UniquePaths(int m, int n)
+    {
+        return 0;
+    }
+}
