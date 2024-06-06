a9f6658ec9291b3b1cdb880e21d7b19b00562891
vladg <vlad.grush.99@gmail.com>
2024-06-06 20:21:56 +0300

    preparing to task 63

---
+++ AlgorithmTasks/51-75/UniquePathsTwo.cs
@@ -0,0 +1,24 @@
+﻿namespace AlgorithmTasks._51_75;
+
+public class UniquePathsTwo
+{
+    public void Execute()
+    {
+        var board = new int[][]
+        {
+            new int[] { 0, 0, 0 },
+            new int[] { 0, 1, 0 },
+            new int[] { 0, 0, 0 }
+        };
+        var sol = new Solution();
+        var result = sol.UniquePathsWithObstacles(board);
+        Console.WriteLine(result);
+    }
+}
+
+partial class Solution {
+    public int UniquePathsWithObstacles(int[][] obstacleGrid)
+    {
+        return int.MaxValue;
+    }
+}
