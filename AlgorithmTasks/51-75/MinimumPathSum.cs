561f6d7d33e488f7b5ae4701c2569c222ed20b7c
vladg <vlad.grush.99@gmail.com>
2024-06-08 19:56:43 +0300

    preparing to task 64

---
+++ AlgorithmTasks/51-75/MinimumPathSum.cs
@@ -0,0 +1,25 @@
+﻿namespace AlgorithmTasks._51_75;
+
+public class MinimumPathSum
+{
+    public void Execute()
+    {
+        var grid = new int[][]
+        {
+            new[] { 1, 3, 1 },
+            new[] { 1, 5, 1 },
+            new[] { 4, 2, 1 }
+        };
+        var sol = new Solution();
+        var result = sol.MinPathSum(grid);
+        Console.WriteLine(result);
+    }
+}
+
+partial class Solution
+{
+    public int MinPathSum(int[][] grid)
+    {
+        return 0;
+    }
+}
