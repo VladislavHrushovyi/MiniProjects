2f5539bda8dbd609ec05298e847b0f5adf40c130
vladg <vlad.grush.99@gmail.com>
2024-06-10 20:49:40 +0300

    preparing to task 65

---
+++ AlgorithmTasks/51-75/ValidNumber.cs
@@ -0,0 +1,18 @@
+﻿namespace AlgorithmTasks._51_75;
+
+public class ValidNumber
+{
+    public void Execute()
+    {
+        var sol = new Solution();
+        var result = sol.IsNumber("0");
+        Console.WriteLine(result);
+    }
+}
+
+partial class Solution {
+    public bool IsNumber(string s)
+    {
+        return true;
+    }
+}
