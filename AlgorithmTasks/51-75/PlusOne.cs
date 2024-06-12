5e9df90b122d148f1f5dab394a5d75be451c6cf0
vladg <vlad.grush.99@gmail.com>
2024-06-12 18:58:31 +0300

    preparing to task 66

---
+++ AlgorithmTasks/51-75/PlusOne.cs
@@ -0,0 +1,19 @@
+﻿namespace AlgorithmTasks._51_75;
+
+public class PlusOne
+{
+    public void Execute()
+    {
+        var number = new int[] { 1, 2, 3 };
+        var sol = new Solution();
+        var result = sol.PlusOne(number);
+        Console.WriteLine("[" + string.Join(", ", result) + "]");
+    }
+}
+
+partial class Solution {
+    public int[] PlusOne(int[] digits)
+    {
+        return new[] { 1 };
+    }
+}
