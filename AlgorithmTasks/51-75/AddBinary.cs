96e1d922b9c95a63130d162764bb6bc7d12df872
vladg <vlad.grush.99@gmail.com>
2024-06-14 19:35:30 +0300

    preparing to task 67

---
+++ AlgorithmTasks/51-75/AddBinary.cs
@@ -0,0 +1,17 @@
+﻿namespace AlgorithmTasks._51_75;
+
+public class AddBinary
+{
+    public void Execute()
+    {
+        var sol = new Solution();
+        var result = sol.AddBinary("11", "1");
+        Console.WriteLine(result);
+    }
+}
+
+partial class Solution {
+    public string AddBinary(string a, string b) {
+        return String.Empty;
+    }
+}
