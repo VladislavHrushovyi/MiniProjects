76d834c1c63387dd1ab0c9798ebca25d7baa3152
vladg <vlad.grush.99@gmail.com>
2024-06-17 21:45:24 +0300

    preparing to task 68

---
+++ AlgorithmTasks/51-75/TextJustification.cs
@@ -0,0 +1,19 @@
+﻿namespace AlgorithmTasks._51_75;
+
+public class TextJustification
+{
+    public void Execute()
+    {
+        var data = new string[] { "This", "is", "an", "example", "of", "text", "justification." };
+        var sol = new Solution();
+        var result = sol.FullJustify(data, 16);
+        Console.WriteLine(string.Join(",\n", result));
+    }
+}
+
+partial class Solution {
+    public IList<string> FullJustify(string[] words, int maxWidth)
+    {
+        return new List<string>();
+    }
+}
