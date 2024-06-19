9110542fff8496633d42e402ed532147446c51f8
vladg <vlad.grush.99@gmail.com>
2024-06-19 19:24:51 +0300

    preparing to task 69

---
+++ AlgorithmTasks/51-75/Sqrt(x).cs
@@ -0,0 +1,18 @@
+﻿namespace AlgorithmTasks._51_75;
+
+public class Sqrt_x_
+{
+    public void Execute()
+    {
+        var sol = new Solution();
+        var result = sol.MySqrt(4);
+        Console.WriteLine(result);
+    }
+}
+
+partial class Solution {
+    public int MySqrt(int x)
+    {
+        return 0;
+    }
+}
