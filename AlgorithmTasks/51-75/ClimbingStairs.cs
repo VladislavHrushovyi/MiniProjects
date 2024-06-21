9535d94ceafb80e244cfecaf5202cff26d0043f6
vladg <vlad.grush.99@gmail.com>
2024-06-21 20:25:48 +0300

    preparing to task 70

---
+++ AlgorithmTasks/51-75/ClimbingStairs.cs
@@ -0,0 +1,18 @@
+﻿namespace AlgorithmTasks._51_75;
+
+public class ClimbingStairs
+{
+    public void Execute()
+    {
+        var sol = new Solution();
+        var result = sol.ClimbStairs(2);
+        Console.WriteLine(result);
+    }
+}
+
+partial class Solution {
+    public int ClimbStairs(int n)
+    {
+        return 0;
+    }
+}
