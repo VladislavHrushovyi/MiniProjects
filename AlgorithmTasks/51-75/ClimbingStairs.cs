493e2299caf101f79b03c8ee676f8a396cf923b5
vladg <vlad.grush.99@gmail.com>
2024-06-22 20:13:16 +0300

    LeetCode 70. Climbing Stairs

--- AlgorithmTasks/51-75/ClimbingStairs.cs
+++ AlgorithmTasks/51-75/ClimbingStairs.cs
@@ -13,6 +13,11 @@ public class ClimbingStairs
 partial class Solution {
     public int ClimbStairs(int n)
     {
-        return 0;
+        int[] tab = new int[n + 1];
+        if (tab.Length > 0) tab[0] = 1;
+        if (tab.Length > 1) tab[1] = 1;
+        for (int i = 2; i < tab.Length; i++) 
+            tab[i] = tab[i - 1] + tab[i - 2];
+        return tab[n];
     }
 }
--- AlgorithmTasks/Program.cs
+++ AlgorithmTasks/Program.cs
@@ -1,5 +1,5 @@
 ﻿using AlgorithmTasks._26_50;
 using AlgorithmTasks._51_75;
 
-var task = new Sqrt_x_();
+var task = new ClimbingStairs();
 task.Execute();
