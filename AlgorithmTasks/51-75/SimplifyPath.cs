150c0be8d9b8191aedaa7ddf6694662b41b35b2b
vladg <vlad.grush.99@gmail.com>
2024-06-23 19:38:12 +0300

    preparing to task 71

---
+++ AlgorithmTasks/51-75/SimplifyPath.cs
@@ -0,0 +1,18 @@
+﻿namespace AlgorithmTasks._51_75;
+
+public class SimplifyPath
+{
+    public void Execute()
+    {
+        var sol = new Solution();
+        var result = sol.SimplifyPath("/home");
+        Console.WriteLine(result);
+    }
+}
+
+partial class Solution {
+    public string SimplifyPath(string path)
+    {
+        return string.Empty;
+    }
+}
