7ad6e6c1a46bf3a684085426899acf591ed5895e
vladg <vlad.grush.99@gmail.com>
2024-06-24 20:25:30 +0300

    LeetCode 71. Simplify Path

--- AlgorithmTasks/51-75/SimplifyPath.cs
+++ AlgorithmTasks/51-75/SimplifyPath.cs
@@ -1,4 +1,6 @@
-﻿namespace AlgorithmTasks._51_75;
+﻿using System.Text;
+
+namespace AlgorithmTasks._51_75;
 
 public class SimplifyPath
 {
@@ -13,6 +15,29 @@ public class SimplifyPath
 partial class Solution {
     public string SimplifyPath(string path)
     {
-        return string.Empty;
+        Stack<string> st = new Stack<string>();
+        path = path.Replace("//", "/").TrimEnd('/');
+
+        foreach (string s in path.Split('/', StringSplitOptions.RemoveEmptyEntries))
+        {
+            if (s == ".." && st.Count > 0)
+                st.Pop();
+            else if (s is "." or "..")
+                continue;
+            else
+                st.Push(s);
+        }
+
+        StringBuilder sb = new StringBuilder();
+
+        while (st.Count > 0)
+        {
+            sb.Insert(0, "/" + st.Pop());
+        }
+
+        if (sb.Length == 0)
+            sb.Append("/");
+
+        return sb.ToString();
     }
 }
--- AlgorithmTasks/Program.cs
+++ AlgorithmTasks/Program.cs
@@ -1,5 +1,5 @@
 ﻿using AlgorithmTasks._26_50;
 using AlgorithmTasks._51_75;
 
-var task = new ClimbingStairs();
+var task = new SimplifyPath();
 task.Execute();
