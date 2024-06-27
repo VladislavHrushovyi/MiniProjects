a86166984093025ef3cefb2c11a2b5598c57bb74
vladg <vlad.grush.99@gmail.com>
2024-06-27 21:26:36 +0300

    extract token to constructor

--- MintTreeSearcher/MintRequestSender.cs
+++ MintTreeSearcher/MintRequestSender.cs
@@ -4,40 +4,53 @@ using System.Text.Json;
 
 namespace MintTreeSearcher;
 
-public class MintRequestSender
+public class MintRequestSender(string authToken)
 {
-
     public async Task<Response> GetNotClaimedMintTree(int id)
     {
         var userInfo = await GetUserInfo(id);
         var uri = new Uri($"https://www.mintchain.io/api/tree/steal/energy-list?id={userInfo.Result.Id}");
         using var httpClient = new HttpClient();
-        httpClient.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("Bearer",
-            "eyJhbGciOiJIUzI1NiJ9.eyJhZGRyZXNzIjoiMHgwNjEwZjVlOTAzNTIwYjNhMTAxMzQ0NWEyYzEyZWY1Y2I2NGIzNGEyIiwidWlkIjo2NjgyNTUsImV4cCI6MTcyMDAyMjc4M30.uHX4mQaVNIrahwW1fA6jx708-1VvTL1DqDQjH6Bk2Ts");
-        var response = await httpClient.GetAsync(uri);
-        var jsonString = await response.Content.ReadAsStringAsync();
-        var claimableInfo = JsonSerializer.Deserialize<Response>(jsonString);
+        httpClient.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("Bearer", authToken);
+        try
+        {
+            var response = await httpClient.GetAsync(uri);
+            if (response.StatusCode == HttpStatusCode.OK)
+            {
+                var jsonString = await response.Content.ReadAsStringAsync();
+                var claimableInfo = JsonSerializer.Deserialize<Response>(jsonString);
 
-        return claimableInfo;
+                return claimableInfo;
+            }
+        }
+        catch (Exception e)
+        {
+            Console.WriteLine(e.Message);
+        }
+
+        return new Response() { Result = new ItemsTree[] { new ItemsTree() { Amount = 1, Stealable = false } } };
     }
 
     private async Task<UserInfo> GetUserInfo(int treeId)
     {
         var uri = new Uri($"https://www.mintchain.io/api/tree/user-info?treeid={treeId}");
         using var httpClient = new HttpClient();
-        httpClient.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("Bearer",
-            "eyJhbGciOiJIUzI1NiJ9.eyJhZGRyZXNzIjoiMHgwNjEwZjVlOTAzNTIwYjNhMTAxMzQ0NWEyYzEyZWY1Y2I2NGIzNGEyIiwidWlkIjo2NjgyNTUsImV4cCI6MTcyMDAyMjc4M30.uHX4mQaVNIrahwW1fA6jx708-1VvTL1DqDQjH6Bk2Ts");
-        var response = await httpClient.GetAsync(uri);
-        var jsonString = await response.Content.ReadAsStringAsync();
+        httpClient.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("Bearer", authToken);
         try
         {
-            var objResult = JsonSerializer.Deserialize<UserInfo>(jsonString);
-            return objResult;
+            var response = await httpClient.GetAsync(uri);
+            if (response.StatusCode == HttpStatusCode.OK)
+            {
+                var jsonString = await response.Content.ReadAsStringAsync();
+                var objResult = JsonSerializer.Deserialize<UserInfo>(jsonString);
+                return objResult;
+            }
         }
         catch (Exception e)
         {
-            Console.WriteLine(jsonString);
-            throw;
+            Console.WriteLine(e.Message);
         }
+
+        return new UserInfo() { Result = new Result() { Id = 0 } };
     }
 }
