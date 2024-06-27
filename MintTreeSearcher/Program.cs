5c38ea669eb799997718c9f728523cd89f0a8329
vladg <vlad.grush.99@gmail.com>
2024-06-27 21:27:27 +0300

    implement parallel sending requests

--- MintTreeSearcher/Program.cs
+++ MintTreeSearcher/Program.cs
@@ -1,33 +1,41 @@
 ﻿using System.Text;
-using System.Text.Json;
 using MintTreeSearcher;
 
-MintRequestSender mintClient = new MintRequestSender();
 if (File.Exists("./Trees.txt"))
 {
     File.Delete("./Trees.txt");
 }
 
 await using var file = File.Create("./Trees.txt");
-Console.WriteLine("From");
+
+Console.WriteLine("Enter auth token:");
+var authToken = Console.ReadLine();
+
+Console.WriteLine("From:");
 int from = Int32.Parse(Console.ReadLine());
-Console.WriteLine("To");
+
+Console.WriteLine("To:");
 int to = Int32.Parse(Console.ReadLine());
 
-for (int i = from; i <= to; i++)
+Console.WriteLine("Enter more than:");
+int moreThan = Int32.Parse(Console.ReadLine());
+
+MintRequestSender mintClient = new MintRequestSender(authToken);
+
+Parallel.For(from, to,new ParallelOptions(){MaxDegreeOfParallelism = 8}, i =>
 {
-    var claimableInfo = await mintClient.GetNotClaimedMintTree(i);
-    if (claimableInfo.Result != null)
-    {
-        var validObj = claimableInfo.Result.FirstOrDefault(x => x is { Stealable: true, Amount: >= 100 });
-        if (validObj is not null)
-        {
-            Console.WriteLine($"https://www.mintchain.io/mint-forest?id={i} ---> {validObj.Amount}ME");
-            if (validObj.Amount >= 800)
-            {
-                string line = $"https://www.mintchain.io/mint-forest?id={i} ---> {validObj.Amount}ME \n";
-                file.Write(Encoding.ASCII.GetBytes(line));   
-            }
-        }   
-    }
-}
+    var claimableInfo = mintClient.GetNotClaimedMintTree(i).GetAwaiter().GetResult();
+     if (claimableInfo.Result != null)
+     {
+         var validObj = claimableInfo.Result.FirstOrDefault(x => x is { Stealable: true, Amount: >= 100 });
+         if (validObj is not null)
+         {
+             Console.WriteLine($"https://www.mintchain.io/mint-forest?id={i} ---> {validObj.Amount}ME");
+             if (validObj.Amount >= moreThan)
+             {
+                 string line = $"https://www.mintchain.io/mint-forest?id={i} ---> {validObj.Amount}ME \n";
+                 file.Write(Encoding.ASCII.GetBytes(line));   
+             }
+         }   
+     }
+});
