70dff6436f508d468e52f690158b4ffea7ce9deb
vladg <vlad.grush.99@gmail.com>
2024-06-28 18:21:44 +0300

    fix bug

--- MintTreeSearcher/Program.cs
+++ MintTreeSearcher/Program.cs
@@ -6,7 +6,7 @@ if (File.Exists("./Trees.txt"))
     File.Delete("./Trees.txt");
 }
 
-await using var file = File.Create("./Trees.txt");
+//using var file = File.AppendText("./Trees.txt");
 
 Console.WriteLine("Enter auth token:");
 var authToken = Console.ReadLine();
@@ -22,20 +22,41 @@ int moreThan = Int32.Parse(Console.ReadLine());
 
 MintRequestSender mintClient = new MintRequestSender(authToken);
 
-Parallel.For(from, to,new ParallelOptions(){MaxDegreeOfParallelism = 8}, i =>
+void DoSearch(int id)
 {
-    var claimableInfo = mintClient.GetNotClaimedMintTree(i).GetAwaiter().GetResult();
-     if (claimableInfo.Result != null)
-     {
-         var validObj = claimableInfo.Result.FirstOrDefault(x => x is { Stealable: true, Amount: >= 100 });
-         if (validObj is not null)
-         {
-             Console.WriteLine($"https://www.mintchain.io/mint-forest?id={i} ---> {validObj.Amount}ME");
-             if (validObj.Amount >= moreThan)
-             {
-                 string line = $"https://www.mintchain.io/mint-forest?id={i} ---> {validObj.Amount}ME \n";
-                 file.Write(Encoding.ASCII.GetBytes(line));   
-             }
-         }   
-     }
-});
+    var claimableInfo = mintClient.GetNotClaimedMintTree(id).GetAwaiter().GetResult();
+    if (claimableInfo.Result != null)
+    {
+        var validObj = claimableInfo.Result.FirstOrDefault(x => x is { Stealable: true, Amount: >= 100 });
+        if (validObj is not null)
+        {
+            Console.WriteLine($"https://www.mintchain.io/mint-forest?id={id} ---> {validObj.Amount}ME");
+            if (validObj.Amount >= moreThan)
+            {
+                string line = $"https://www.mintchain.io/mint-forest?id={id} ---> {validObj.Amount}ME \n";
+                File.AppendAllText("Trees.txt",line);
+            }
+        }   
+    }
+}
+Parallel.For(from, to,new ParallelOptions(){MaxDegreeOfParallelism = 8}, _ => DoSearch(Random.Shared.Next(from,to)));
+// async Task DoSearchAsync(int id)
+// {
+//     var claimableInfo = await mintClient.GetNotClaimedMintTree(id);
+//     if (claimableInfo.Result != null)
+//     {
+//         var validObj = claimableInfo.Result.FirstOrDefault(x => x is { Stealable: true, Amount: >= 100 });
+//         if (validObj is not null)
+//         {
+//             Console.WriteLine($"https://www.mintchain.io/mint-forest?id={id} ---> {validObj.Amount}ME");
+//             if (validObj.Amount >= moreThan)
+//             {
+//                 string line = $"https://www.mintchain.io/mint-forest?id={id} ---> {validObj.Amount}ME \n";
+//                 file.Write(Encoding.ASCII.GetBytes(line));   
+//             }
+//         }   
+//     }
+// }
+//
+// var reqTasks = Enumerable.Range(from, to).Select(DoSearchAsync);
+// await Task.WhenAll(reqTasks);
