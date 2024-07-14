d6d7db7313880844450018bb7fbc5b21f1aa04d1
vladg <vlad.grush.99@gmail.com>
2024-07-14 19:52:28 +0300

    Create new proj for checking leaderboards trees

---
+++ MintForestBase/Models/LeaderbordTrees.cs
@@ -0,0 +1,17 @@
+﻿using System.Text.Json.Serialization;
+
+namespace MintForestBase.Models;
+
+public class LeaderboardTrees
+{
+    [JsonPropertyName("result")]
+    public IEnumerable<UserLeaderboard> Result { get; set; }
+}
+
+public class UserLeaderboard
+{
+    [JsonPropertyName("id")]
+    public int Id { get; set; }
+    [JsonPropertyName("treeId")]
+    public int TreeId { get; set; }
+}
