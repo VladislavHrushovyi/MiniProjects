d6422107b237152fc417c49d7f6c215411ed51a8
vladg <vlad.grush.99@gmail.com>
2024-07-19 20:02:02 +0300

    LeetCode 72. Edit Distance

--- AlgorithmTasks/51-75/EditDistance.cs
+++ AlgorithmTasks/51-75/EditDistance.cs
@@ -10,9 +10,40 @@ public class EditDistance
     }
 }
 
-partial class Solution {
+partial class Solution
+{
     public int MinDistance(string word1, string word2)
     {
-        return 0;
+        var m = word1.Length;
+        var n = word2.Length;
+
+        var dp = new int[m + 1, n + 1];
+
+        for (int i = 0; i <= m; i++)
+        {
+            dp[i, 0] = i;
+        }
+
+        for (int j = 0; j <= n; j++)
+        {
+            dp[0, j] = j;
+        }
+
+        for (int i = 1; i <= m; i++)
+        {
+            for (int j = 1; j <= n; j++)
+            {
+                if (word1[i - 1] == word2[j - 1])
+                {
+                    dp[i, j] = dp[i - 1, j - 1];
+                }
+                else
+                {
+                    dp[i, j] = Math.Min(Math.Min(dp[i - 1, j], dp[i, j - 1]), dp[i - 1, j - 1]) + 1;
+                }
+            }
+        }
+
+        return dp[m, n];
     }
 }
