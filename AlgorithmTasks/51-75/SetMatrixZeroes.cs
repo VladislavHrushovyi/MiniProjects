c522d42e007dadeec94a1e2c65cc9550192bd07a
vladg <vlad.grush.99@gmail.com>
2024-07-20 19:53:15 +0300

    preparing to task 73

---
+++ AlgorithmTasks/51-75/SetMatrixZeroes.cs
@@ -0,0 +1,29 @@
+﻿namespace AlgorithmTasks._51_75;
+
+public class SetMatrixZeroes
+{
+    public void Execute()
+    {
+        var matrix = new int[][]
+        {
+            new int[] { 1, 1, 1 },
+            new int[] { 1, 0, 1 },
+            new int[] { 1, 1, 1 }
+        };
+        var sol = new Solution();
+        sol.SetZeroes(matrix);
+
+        Console.Write("[");
+        foreach (var line in matrix)
+        {
+            Console.Write("["+string.Join(", ", line)+"]");
+        }
+        Console.Write("]");
+    }
+}
+
+partial class Solution {
+    public void SetZeroes(int[][] matrix) {
+        
+    }
+}
