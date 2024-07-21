14880cf910f5120f71a8d00116853b82249c57f0
vladg <vlad.grush.99@gmail.com>
2024-07-21 20:42:04 +0300

    preparing to new proj

---
+++ MintForestBase/Extension/HttpClientExtension.cs
@@ -0,0 +1,29 @@
+﻿using System.Net;
+
+namespace MintForestBase.Extension;
+
+public static class HttpClientExtension
+{
+    public static void SetProxy(this HttpClient client, string proxyString)
+    {
+        var proxySplit = proxyString.Split(":");
+        var proxy = new WebProxy()
+        {
+            Address = new Uri($"http://{proxySplit[0]}:{proxySplit[1]}"),
+            BypassProxyOnLocal = false,
+            UseDefaultCredentials = false,
+            
+            Credentials = new NetworkCredential()
+            {
+                UserName = proxySplit[2],
+                Password = proxySplit[3]
+            }
+            
+        };
+        
+        var httpClientHandler = new HttpClientHandler()
+        {
+            Proxy = proxy
+        };
+    }
+}
