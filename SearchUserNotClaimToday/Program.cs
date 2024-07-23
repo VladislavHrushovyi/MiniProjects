90b2768c690a088959cae6a1e94bc026797a6ab5
vladg <vlad.grush.99@gmail.com>
2024-07-23 19:58:43 +0300

    write todo list for project

--- SearchUserNotClaimToday/Program.cs
+++ SearchUserNotClaimToday/Program.cs
@@ -1,3 +1,10 @@
 ﻿// See https://aka.ms/new-console-template for more information
 
 Console.WriteLine("Hello, World!");
+/*
+ * 1 read auth token
+ * 2 read proxy and create http clients
+ * 3 fetch leaderboard users
+ * 4 filter by not claim today and after claim more than 10_000 or steeling more 5000ME
+ * write user id into file
+*/
