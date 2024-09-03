acd9a89b126bba29c58a415b8c6dcb4e890bd3ae
vladg <vlad.grush.99@gmail.com>
2024-09-03 22:18:06 +0300

    trying interacting with smart contract

---
+++ Web3Interacting/ContractInteracting.cs
@@ -0,0 +1,51 @@
+﻿using System.Numerics;
+using Nethereum.ABI.FunctionEncoding.Attributes;
+using Nethereum.Contracts;
+using Nethereum.Hex.HexTypes;
+using Nethereum.Web3;
+using Nethereum.Web3.Accounts;
+
+namespace Web3Interacting;
+
+public class ContractInteracting
+{
+    private readonly Web3 web3;
+    private readonly string proxyContractAddress;
+
+    public ContractInteracting(string nodeUrl, string privateKey, string proxyContractAddress)
+    {
+        var account = new Account(privateKey);
+        this.web3 = new Web3(account, nodeUrl);
+        this.proxyContractAddress = proxyContractAddress;
+    }
+
+    [Function("steal")]
+    public class StealFunction : FunctionMessage
+    {
+        [Parameter("address", "target", 1)] public string Target { get; set; }
+
+        [Parameter("uint64", "time", 2)] public long Time { get; set; }
+
+        [Parameter("uint256", "point", 3)] public BigInteger Point { get; set; }
+
+        [Parameter("bytes", "signature", 4)] public byte[] Signature { get; set; }
+    }
+
+    public async Task<string> StealAsync(string target, long time, BigInteger point, byte[] signature)
+    {
+        var stealFunction = new StealFunction
+        {
+            Target = target,
+            Time = time,
+            Point = point,
+            Signature = signature,
+            Gas = new HexBigInteger(300000), // Збільшено ліміт газу
+            GasPrice = new HexBigInteger(Web3.Convert.ToWei(0.01, Nethereum.Util.UnitConversion.EthUnit.Gwei)) // Встановлення ціни газу
+        };
+
+        var handler = web3.Eth.GetContractTransactionHandler<StealFunction>();
+        var transactionReceipt =
+            await handler.SendRequestAsync(proxyContractAddress, stealFunction);
+        return transactionReceipt;
+    }
+}
---
+++ Web3Interacting/SignatureGenerator.cs
@@ -0,0 +1,119 @@
+﻿using Nethereum.ABI;
+using Nethereum.Hex.HexConvertors.Extensions;
+using Nethereum.Signer;
+using System.Numerics;
+using Nethereum.ABI.EIP712;
+using Nethereum.ABI.FunctionEncoding.Attributes;
+using Nethereum.Util;
+
+namespace Web3Interacting;
+
+public class SignatureGenerator
+{
+     public class Eip712Domain : Domain
+    {
+        public string Name { get; set; }
+        public string Version { get; set; }
+        public BigInteger ChainId { get; set; }
+        public string VerifyingContract { get; set; }
+    }
+
+    [Struct("StealParams")]
+    public class StealParams
+    {
+        [Parameter("address", "user", 1)]
+        public string User { get; set; }
+
+        [Parameter("address", "target", 2)]
+        public string Target { get; set; }
+
+        [Parameter("uint64", "time", 3)]
+        public long Time { get; set; }
+
+        [Parameter("uint256", "point", 4)]
+        public BigInteger Point { get; set; }
+    }
+
+    private string GetDomainSeparator(Eip712Domain domain)
+    {
+        var abiEncode = new ABIEncode();
+        var encoded = abiEncode.GetSha3ABIEncodedPacked(
+            new ABIValue("bytes32", new Sha3Keccack().CalculateHash("EIP712Domain(string name,string version,uint256 chainId,address verifyingContract)")),
+            new ABIValue("bytes32", new Sha3Keccack().CalculateHash(domain.Name)),
+            new ABIValue("bytes32", new Sha3Keccack().CalculateHash(domain.Version)),
+            new ABIValue("uint256", domain.ChainId),
+            new ABIValue("address", domain.VerifyingContract)
+        );
+        return new Sha3Keccack().CalculateHash(encoded).ToHex();
+    }
+
+    private string GetTypeHash(StealParams parameters)
+    {
+        return new Sha3Keccack().CalculateHash("StealParams(address user,address target,uint64 time,uint256 point)");
+    }
+
+    private string GetStructHash(StealParams parameters)
+    {
+        var abiEncode = new ABIEncode();
+        var encoded = abiEncode.GetSha3ABIEncodedPacked(
+            new ABIValue("bytes32", GetTypeHash(parameters)),
+            new ABIValue("address", parameters.User),
+            new ABIValue("address", parameters.Target),
+            new ABIValue("uint64", parameters.Time),
+            new ABIValue("uint256", parameters.Point)
+        );
+        return new Sha3Keccack().CalculateHash(encoded).ToHex();
+    }
+
+    private byte[] GetMessageHashEip712(StealParams parameters, Eip712Domain domain)
+    {
+        var domainSeparator = GetDomainSeparator(domain);
+        var structHash = GetStructHash(parameters);
+
+        var abiEncode = new ABIEncode();
+        var encoded = abiEncode.GetSha3ABIEncodedPacked(
+            new ABIValue("bytes2", "0x1901"), // EIP-712 префікс
+            new ABIValue("bytes32", domainSeparator),
+            new ABIValue("bytes32", structHash)
+        );
+
+        return new Sha3Keccack().CalculateHash(encoded);
+    }
+
+    private byte[] SignMessage(byte[] messageHash, string privateKey)
+    {
+        var signer = new EthereumMessageSigner();
+        var signature = signer.Sign(messageHash, privateKey);
+        
+        return signature.HexToByteArray();
+    }
+
+    public byte[] GenerateSignature(string user, string target, long time, BigInteger point, string privateKey, string contractAddress)
+    {
+        // Створюємо домен EIP-712
+        var domain = new Eip712Domain
+        {
+            Name = "MintMainnet",  // Ім'я вашого домену
+            Version = "1",
+            ChainId = 185, // ChainId мережі, у якій ви працюєте (наприклад, 1 для Ethereum mainnet)
+            VerifyingContract = contractAddress // Адреса контракту
+        };
+
+        // Створюємо параметри
+        var parameters = new StealParams
+        {
+            User = user,
+            Target = target,
+            Time = time,
+            Point = point
+        };
+
+        // Отримання хешу повідомлення за допомогою EIP-712
+        var messageHash = GetMessageHashEip712(parameters, domain);
+
+        // Підписання хешу приватним ключем
+        var signature = SignMessage(messageHash, privateKey);
+
+        return signature;
+    }
+}
