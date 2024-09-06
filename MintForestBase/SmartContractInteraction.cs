b1e4040d8f505fa5ef32d1357875d415f0871d33
vladg <vlad.grush.99@gmail.com>
2024-09-06 18:32:44 +0300

    add lock statement for contract interaction

--- MintForestBase/SmartContractInteraction.cs
+++ MintForestBase/SmartContractInteraction.cs
@@ -9,6 +9,7 @@ namespace MintForestBase;
 
 public class SmartContractInteraction
 {
+    private object _lock = new();
     private readonly Account _account;
     private readonly Web3 _web3;
     private const string ContractAddress = "0x12906892AaA384ad59F2c431867af6632c68100a";
@@ -21,36 +22,39 @@ public class SmartContractInteraction
 
     public async Task<bool> StealActionInteraction(ProofModel proofModel)
     {
-        var nonce = await _web3.Eth.Transactions.GetTransactionCount.SendRequestAsync(_account.Address);
-
-        var gasPrice = Web3.Convert.ToWei(0.0001, UnitConversion.EthUnit.Gwei);
-        var gasLimit = 50000;
-
-        var transactionInput = new TransactionInput()
+        lock (_lock)
         {
-            From = _account.Address,
-            To = ContractAddress,
-            Data = proofModel.Result.Tx,
-            Gas = new HexBigInteger(gasLimit),
-            GasPrice = new HexBigInteger(gasPrice),
-            Nonce = new HexBigInteger(nonce)
-        };
+            var nonce = _web3.Eth.Transactions.GetTransactionCount.SendRequestAsync(_account.Address);
 
-        try
-        {
-            var transactionSigner = new AccountOfflineTransactionSigner();
-            var chainId = 185;
+            var gasPrice = Web3.Convert.ToWei(0.01, UnitConversion.EthUnit.Gwei);
+            var gasLimit = 100000;
+
+            var transactionInput = new TransactionInput()
+            {
+                From = _account.Address,
+                To = ContractAddress,
+                Data = proofModel.Result.Tx,
+                Gas = new HexBigInteger(gasLimit),
+                GasPrice = new HexBigInteger(gasPrice),
+                Nonce = new HexBigInteger(nonce.Result)
+            };
+
+            try
+            {
+                var transactionSigner = new AccountOfflineTransactionSigner();
+                var chainId = 185;
             
-            var rawTransaction = transactionSigner.SignTransaction(_account, transactionInput, chainId);
+                var rawTransaction = transactionSigner.SignTransaction(_account, transactionInput, chainId);
 
-            var txHash = await _web3.Eth.Transactions.SendRawTransaction.SendRequestAsync("0x" + rawTransaction);
-            Console.WriteLine($"Transaction Hash: {txHash}");
-        }
-        catch (Exception ex)
-        {
-            Console.WriteLine($"Error: {ex.Message}");
-        }
+                var txHash = _web3.Eth.Transactions.SendRawTransaction.SendRequestAsync("0x" + rawTransaction);
+                Console.WriteLine($"Transaction Hash: {txHash.Result}");
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"Error: {ex.Message}");
+            }
 
-        return true;
+            return true;   
+        }
     }
 }
